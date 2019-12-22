da615e7c8c9c95b709682f980666f99c5d5a7582
Teanlouise <teanmcgrath@gmail.com>
2019-12-22 13:20:03 +1000

    Complete steps 5-8 & 10-11 (Part B - Exoplanets) - add functions - get_r_exo, get_dist_exo_star, get_veloctity_exo, get_exo_outputs

--- InteractiveSpaceAliens42637460.py
+++ InteractiveSpaceAliens42637460.py
@@ -5,6 +5,7 @@ from pylab import *
 
 ######################  functions are defined here ###########################
 
+# PROVIDED FUNCTIONS
 def get_period_of_planet(dist_exo_star, velocity_exo):
     """ Determine the period of an exoplanet using the distance of the planet
     from its star and the velocity of the planet. The period is the time, in
@@ -21,7 +22,6 @@ def get_period_of_planet(dist_exo_star, velocity_exo):
         flt: The period of the exoplanet (s)
     """
     return (2 * math.pi * dist_exo_star / velocity_exo)
-
 
 def get_transit_time(velocity_exo, r_star):
     """ Determine the transit time of the exoplanet using its velocity and the
@@ -40,7 +40,6 @@ def get_transit_time(velocity_exo, r_star):
     """
     return (2 * r_star) / velocity_exo
 
-
 def get_min_rel_intensity(r_exo, r_star):
     """ Determine the minimum relative intensity of the exoplanet's star from an
     observed point on Earth. The minimum intensity is when the exoplanet is
@@ -58,7 +57,7 @@ def get_min_rel_intensity(r_exo, r_star):
     """
     return 1 - ((r_exo / r_star) ** 2)
 
-
+# ADDITIONAL FUNCTIONS
 def drake_equation(c):
     """ The drake equation is used as a guide to determine the number of civilisations
     in our galaxy in which we could communicate (N).
@@ -70,17 +69,94 @@ def drake_equation(c):
             (estimate (1960 and recent) : 0.5)
         n = number of planets per solar system with conditions suitable for life
             (estimates: 1960 = 2, recent = 1)
-        L = average lifetime (in years) of such a civilisation within the detection window
+        L = average lifetime (years) of a civilisation within the detection window
             (estimate: 10)
 
     Paramaters:
-        c (flt): proportion of potentially habitable planets on which a techonological civilisation develops
+        c (flt): proportion of potentially habitable planets on which a
+        techonological civilisation develops
             (estimates: 1960 = 0.0001, recent  0.02)
     Return:
-        flt: N - The number of civilisation in the galaxy that can communicate with Earth
+        flt: Number of civilisation in the galaxy that can communicate with Earth (N)
     """
     return 7 * 0.5 * 1 * 10000 * c
 
+def get_r_exo(user_size):
+    """ Determine the radius of the exoplanet given an input from the user as a
+    proportion of the size of Earth.
+
+    Paramaters:
+        user_size (flt): Proportion of the exoplanet relative to the size of Earth
+    Variables:
+        r_Earth (flt): The radius of the earth (km)
+    Return:
+        flt: The radius of the exoplanet (km)
+    """
+    return r_Earth * user_size
+
+def get_dist_exo_star(user_dist):
+    """ Determine the radius of the exoplanet given an input from the user as a
+    proportion of the distance of the Earth from the sun.
+
+    Paramaters:
+        user_dist (flt): Proportion of the distance of the exoplanet to its star
+            relative to the Earth and the sun
+    Variables:
+        dist_Earth_sun (flt): The distance of the Earth from the sun (km)
+    Return:
+        flt: The distance of the exoplanet from its star(km)
+    """
+    return dist_Earth_sun * user_dist
+
+def get_velocity_exo(user_dist):
+    """ Determine the velocity of the exoplanet given the velocity of the Earth,
+    the distance of the Earth from the sun and the distance of the exoplanet
+    from its star. The velocity is the speed at which it travels.
+
+    Paramaters:
+        user_dist (flt): Proportion of the distance of the exoplanet to its star
+            relative to the Earth and the sun
+    Variables:
+        velocity_Earth (flt): The speed at which the Earthtravels (km/s)
+        dist_Earth_sun (flt): The distance of the Earth from the sun (km)
+        dist_exo_star (flt): The distance of the exoplanet from its star (km)
+    Return:
+        flt: The velocity of the exoplanet (km/s)
+    """
+    dist_exo_star = get_dist_exo_star(user_dist)
+    return velocity_Earth * math.sqrt(dist_Earth_sun / dist_exo_star)
+
+def get_exo_outputs(user_size, user_dist):
+    """ Calculate the outputs of the exoplanets using the patron's input for the
+    size of the exoplanet and the distance of the exoplanet from its star,
+    relative to the Earth and the sun
+
+    Parameters:
+        user_size (flt): Proportion of the exoplanet relative to the size of Earth
+        user_dist (flt): Proportion of the distance of the exoplanet to its star
+            relative to the Earth and the sun
+    Variables:
+        r_exo (float): Radius of the exoplanet, proportional to the Earth
+        dist_exo_star (float): The distance of the exoplanet to its star,
+            proportational to Earth
+        velocity_exo (float): The velocity of the exoplanet
+        outputs:Array containing these parameters of the exoplanet
+    Return:
+        array: outputs containing appropriately converted period, transit_time
+            and min_intensity of the exoplanet
+    """
+
+    r_exo = get_r_exo(user_size)
+    dist_exo_star = get_dist_exo_star(user_dist)
+    velocity_exo = get_velocity_exo(user_dist)
+    outputs = ["period", "transit_time", "min_intensity"]
+
+    # Period - convert from seconds to days
+    outputs[0] = float(get_period_of_planet(dist_exo_star, velocity_exo))/86400
+    # Transit time - convert from seconds to hours
+    outputs[1] = float(get_transit_time(velocity_exo, r_star))/3600
+    outputs[2] = float(get_min_rel_intensity(r_exo, r_star))
+    return outputs
 
 ######################  some constants used throughout ###########################
 
@@ -126,6 +202,9 @@ def main():
         patron_type (int): Whether the user is an enthusasist or a rookie
         search (Boolean):
      """
+    # Define constant to allow user to continuously search for exoplanets in step 11
+    search = 1
+
     # Step 1
     # todo: Write introduction to exhibit
     print("Welcome to 'Exploring Our Galaxy'!\n"
@@ -148,6 +227,39 @@ def main():
         # Step 4
         print("\nWith your guess, the number of planets in our galaxy that we could talk to are: ", round(drake_equation(c), 2))
 
+        # Part B - Searching for exoplanets
+        # Step 5
+        print("\n*NOW, LETS FIND SOME NEW PLANETS*\n"
+              "Exoplanets are planets that are not a part of our solar system and so they have their own suns.\n"
+              "We can try and find these other planets by watching these other suns and measuring the light from them.\n"
+              "If the sun has less light then a planet must be blocking it. Like when the moon blocks our sun.\n"
+              "There is a telescope that is very good at finding other planets this way and has found thousands.\n"
+              "\nNow it's your turn to try and find an exoplanet!")
+        # Point where user can search for exoplanets continuously
+        while search == 1:
+            # Step 6
+            user_size = float(input("What is the size of the planet you want to find, compared to Earth? (%)\n"
+                "(50) Half the size\n"
+                "(100) The same size\n"
+                "(200) Double the size\n")) / 100
+            # Step 7
+            user_dist = float(input("How far away from its sun do you want the planet to be, compared to Earth's distance to our sun? (%)\n"
+                "(50) Closer by half\n"
+                "(100) The same distance\n"
+                "(200) Twice as far\n")) / 100
+            # Step 8
+            outputs = get_exo_outputs(user_size, user_dist)
+            print(
+                "\nThere are now three important things we know about your exoplanet: "
+                "\n(1) The time it takes for your planet to go completely around its star (this is 1 year for Earth) is ", round(outputs[0], 2), " days"
+                "\n(2) The time it takes for your planet to move acros its star (the bigger the star, the longer this will be) is", round(outputs[1], 2), " hours"
+                "\n(3) The brightness of your planet's star from Earth when it is being blocked by your planet is ", round(outputs[2], 6))
+
+            # Step 11
+            search = float(input("\nDo you want to search again?\n "
+                                 " (1) Yes\n"
+                                 " (0) NO\n"))
+
     else:
         # ENTHUSIAST
         # Part A - Searching for other civilisations
@@ -163,6 +275,52 @@ def main():
         print("\nUsing your proportion and the most recent estimates for all other factors, the number of civilisations in the galaxy that can communicate with Earth is: ",
             round(drake_equation(c), 2))
 
+        # Part B - Searching for exoplanets
+        # Step 5
+        print("\n*NOW, LETS FIND SOME EXOPLANETS*\n"
+              "\nExoplanets are planets that orbit around stars other than our sun.\n"
+              "We can detect exoplanets by observing the intensity of the light emitted by another star in our galaxy as a function of time.\n"
+              "If an exoplanet passes in front of this star, it partially blocks the star and the measured intensity of the star will slightly decrease.\n"
+              "Multiple measurements at regular intervals can be used to confirm the existence of an exoplanet.\n"
+              "This method has been very succesful at detecting exoplanets. The Kepler space telescope, which uses this approach, has detected several thousand exoplanets.\n"
+              "To model the transit of the exoplanet in front its star, we need to specify the size of the planet and the distance of that planet from its star.\n"
+              "\nNow it's your turn to try and find an exoplanet!")
+        # Point where user can search for exoplanets continuously
+        while search == 1:
+            # Step 6
+            user_size = float(input("What is the size of the planet you want to find, as a percentage of Earth?\n"
+                                    " For example:\n"
+                                    "  (0.5) Half the size of Earth (0.5)\n"
+                                    "  (1) Same size as Earth\n"
+                                    "  (2) Twice as big\n"))
+            # 7. Ask the user for the distance of the planet to its star (relative to the Earth's distance from the sun)
+            user_dist = float(input("How far away from its star do you want the planet to be, as a percentage of Earth's distance to the sun?\n"
+                                    " For example:\n"
+                                    "  (0.5) Closer by half\n"
+                                    "  (1) Same distance\n"
+                                    "  (2) Twice as far\n"))
+            # Step 8
+            outputs = get_exo_outputs(user_size, user_dist)
+            print("\nNow, there are three important factors that need to be calculated to detect your exoplanet:\n"
+                "\n(1) Period of orbit is ", round(outputs[0], 2), " days"
+                "\nThis is the time for the exoplanet to make one complete orbit around its star (this is 1 year for Earth).\n"
+                "The period of orbit is determined by the exoplanet's speed, and the distance the exoplanet is from its star.\n"
+
+                "\n(2) Transit time is ", round(outputs[1], 2), " hours"
+                "\nThe velocity of the exoplanet and the diameter of its star will determine the transit time.\n"
+                "The faster the exoplanet is moving, the shorter the transit time and likewise the larger the diameter of the star, the longer the transit time.\n"
+
+                "\n(3) Minimum relative intensity is ", round(outputs[2], 2),
+                "\nWhen the exoplanet is fully between Eath and the exoplanet's star, the intensity of the star observed from Earth will be decreased as the exoplanet blocks some of the light from the star.\n"
+                "We can define a relative intensity as the ratio of the observed intensity when the exoplanet is in front of the star to the observed intensity when the exoplanet is not in front of the star.")
+
+            # Step 11
+            search = float(input("\nDo you want to search again?\n"
+                                 " (1) Yes\n"
+                                 " (0) NO\n"))
+
+    # Step 12
+    print("Thanks for searching!")
 
 if __name__ == "__main__":
     main()
