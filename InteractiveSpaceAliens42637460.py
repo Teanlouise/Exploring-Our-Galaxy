b625ba1cb9214ab886bd1ba17502850f1ac36de1
Teanlouise <teanmcgrath@gmail.com>
2019-12-22 11:10:01 +1000

    Initial commit - provided program template

---
+++ InteractiveSpaceAliens42637460.py
@@ -0,0 +1,36 @@
+from pylab import *
+
+
+######################  functions are defined here ###########################
+
+# You need to rewrite the body of each of these functions to acieve its purpose
+
+def get_period_of_planet(dist_exo_star, velocity_exo):
+    return 1
+
+
+def get_transit_time(velocity_exo, r_star):
+    return 1
+
+
+def get_min_rel_intensity(r_exo, r_star):
+    return 1
+
+
+######################  some constants used throughout ###########################
+
+# You need to look these up yourself.  Take care with units.
+
+r_star = 1
+
+r_Earth = 1
+
+velocity_Earth = 1
+
+dist_Earth_sun = 1
+
+######################  main body of the code here ###########################
+
+# Write the rest of your code here.  
+# You may call on any of the above functions (and any other functions you chose to write)
+# You may use any of the constants defined above throughout the main body of your code
